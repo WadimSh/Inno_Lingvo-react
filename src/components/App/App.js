b8ff2fc5ceef24b11ce7a4bf39fa590fa5777c4f
WadimSh <wadim11@yandex.ru>
2022-11-07 17:48:02 +0300

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,13 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-import logo from '../../images/logo.png';
-
 import './App.css';
 
 function App() {
   return (
     <div className="page">
-      <img src={logo} />
-      Inno Lingvo
+      <Header />
+      <Footer />
     </div>
   );
 }
