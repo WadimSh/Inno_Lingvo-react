6fa5bae7f2e949d3f14c1a4e769e281517694ab0
WadimSh <wadim11@yandex.ru>
2022-11-07 12:29:54 +0300

    feat: создан проект, подключены шрифты

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
