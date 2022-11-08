e39ef77d96c41914a77ea5ef8c7a23dbc1da96b5
WadimSh <wadim11@yandex.ru>
2022-11-08 18:16:13 +0300

    fix: начата работа над компонентом Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import background from '../../images/background.png'
+
 import './Header.css';
 
 function Header() {
@@ -7,8 +9,14 @@ function Header() {
   return (
     <header className="header wave__wrapper wave__animation">
       <div className="header__line"></div>
-
-      
+      <div className="header__context">
+        <h1 className="header__title">Корректируйте речь и произношение ребёнка, пока занимаетесь своими делами</h1>
+        <p className="header__subtitle">Первый в мире сервис для постановки речи, где детки могут заниматься самостоятельно</p>
+        <span className="header__label">3-7 лет</span>
+        <button className="header__button" type="submit">Получить ранний доступ</button>
+        <span classname="header__discription">400₽/мес на всё время пользования приложением 600₽/мес</span>
+      </div>
+      <img className="header__background" src={background} alt="" />
       <div class="wave__wrapper-inner bgTop">
         <div class="wave wave__top"></div>
       </div>
