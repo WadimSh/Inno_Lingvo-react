32ae02c0d5f5a41bde0c1db80701171f9f93e055
WadimSh <wadim11@yandex.ru>
2022-11-08 10:52:37 +0300

    feat: создан компонент Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,19 @@ function Footer() {
 
   return (
     <footer className="footer">
-      
+      <div className="footer__container">
+        <a className="footer__social" href="#"></a>
+        <a className="footer__social" href="#"></a>
+        <a className="footer__social" href="#"></a>
+      </div>
+      <div className="footer__container">
+        <span className="footer__text">&copy; Inno Lingvo, 2019-{new Date().getFullYear()}</span>
+        <a className="footer__text footer__link" href="#">Политика конфиденциальности</a>
+      </div>
+      <div className="footer__container">
+        <span className="footer__text">Москва, Болотниковская ул. 31</span>
+        <a className="footer__text footer__email" href="mailto: il@innolingvo.ru">il@innolingvo.ru</a>
+      </div>
     </footer>
   )
 }
